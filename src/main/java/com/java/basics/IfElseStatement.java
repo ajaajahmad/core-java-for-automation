b35b1129af3ce1e19f615dae7eae7c460eeafffe
ajaajahmad <ajaaj75@gmail.com>
2025-12-02 22:58:44 +0530

    if-else statement

---
+++ src/main/java/com/java/basics/IfElseStatement.java
@@ -0,0 +1,14 @@
+package com.java.basics;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) {
+
+		if (condition) {
+			// code when condition is true
+		} else {
+			// code when condition is false
+		}
+	}
+
+}
