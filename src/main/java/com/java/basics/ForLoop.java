7919f2ed62e7a479337ff1bf56d22f66aceb489b
ajaajahmad <ajaaj75@gmail.com>
2025-12-02 23:21:41 +0530

    for loop

--- src/main/java/com/java/basics/ForLoop.java
+++ src/main/java/com/java/basics/ForLoop.java
@@ -3,11 +3,11 @@ package com.java.basics;
 public class ForLoop {
 
 	public static void main(String[] args) {
-		
-		for(intialization; condition; increament) {
-			// code to execute.
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
 		}
-		
+
 	}
 
 }
