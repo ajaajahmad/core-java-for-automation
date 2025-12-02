01794ac2504c64162622fa274f1bb57d86e5875c
ajaajahmad <ajaaj75@gmail.com>
2025-12-02 23:11:07 +0530

    "=" and "=="

---
+++ src/main/java/com/java/basics/AssignmentAngCamparison.java
@@ -0,0 +1,11 @@
+package com.java.basics;
+
+public class AssignmentAngCamparison {
+
+	public static void main(String[] args) {
+
+		int x = 20; // assignment "=" is used for assignment.
+		if (x == 20); // comparison "==" is used for comparison.
+	}
+
+}
