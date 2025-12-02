eb94929d94b37670926afb18a86fed5febc39225
ajaajahmad <ajaaj75@gmail.com>
2025-12-02 22:52:42 +0530

    variable and data type

---
+++ src/main/java/com/java/basics/VariableAndDataType.java
@@ -0,0 +1,17 @@
+package com.java.basics;
+
+public class VariableAndDataType {
+
+	public static void main(String[] args) {
+
+		int age = 30;
+		double price = 99.00;
+		char grade = 'B';
+		boolean result = true;
+
+		System.out.println(age);
+		System.out.println(price);
+		System.out.println(grade);
+		System.out.println(result);
+	}
+}
