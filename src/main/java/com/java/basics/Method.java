795de8c0eb304058292456d20049a667bd8cfc8d
ajaajahmad <ajaaj75@gmail.com>
2025-12-03 10:35:22 +0530

    method

---
+++ src/main/java/com/java/basics/Method.java
@@ -0,0 +1,14 @@
+package com.java.basics;
+
+public class Method {
+
+	public static void main(String[] args) {
+		
+		greet();
+	}
+
+	public static void greet() {
+		System.out.println("Hello");
+	}
+
+}
