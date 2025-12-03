2c329ca02161b61e32420eb8f7261b30d15d0db1
ajaajahmad <ajaaj75@gmail.com>
2025-12-03 11:07:37 +0530

    constructor

---
+++ src/main/java/com/java/basics/Constructor.java
@@ -0,0 +1,14 @@
+package com.java.basics;
+
+public class Constructor {
+
+	Constructor() {
+		System.out.println("Constructor is called");
+	}
+
+	public static void main(String[] args) {
+		
+		Constructor ctr =  new Constructor();
+	}
+
+}
