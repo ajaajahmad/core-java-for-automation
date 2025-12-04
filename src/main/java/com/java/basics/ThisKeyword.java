fe05aa2963f05bfa0b5a7a0467e0e95cf3c00b74
ajaajahmad <ajaaj75@gmail.com>
2025-12-04 23:56:05 +0530

    this keyword

---
+++ src/main/java/com/java/basics/ThisKeyword.java
@@ -0,0 +1,19 @@
+package com.java.basics;
+
+public class ThisKeyword {
+
+	int id;
+
+	ThisKeyword(int id) {
+
+		id = id;
+	}
+
+	public static void main(String[] args) {
+		
+		
+		ThisKeyword tk = new ThisKeyword(10);
+		System.out.println(tk.id);
+	}
+
+}
