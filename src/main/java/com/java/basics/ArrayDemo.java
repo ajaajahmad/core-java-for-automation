b8e9fd11bdcd43ba8d633bcd811df5bb546daafc
ajaajahmad <ajaaj75@gmail.com>
2025-12-05 12:59:35 +0530

    array

---
+++ src/main/java/com/java/basics/ArrayDemo.java
@@ -0,0 +1,22 @@
+package com.java.basics;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {10, 20, 30, 40};
+		
+		int[] arr2 = new int[4];
+		
+		arr2[0] = 10;
+		arr2[1] = 20;
+		arr2[2] = 30;
+		arr2[3] = 40;
+		
+		for (int i = 0; i <= arr.length; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
