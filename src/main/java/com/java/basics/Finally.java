8af2e8a2a4d77e34cb590ee7d7346371ee0ca296
Ajaaj Ahmad <ajaaj75@gmail.com>
2025-12-08 00:07:50 +0530

    finally

---
+++ src/main/java/com/java/basics/Finally.java
@@ -0,0 +1,16 @@
+package com.java.basics;
+
+public class Finally {
+
+	public static void main(String[] args) {
+
+		try {
+			int x = 10 / 2;
+		} catch (Exception e) {
+			System.out.println("Exception");
+		} finally {
+			System.out.println("Finally block executed");
+		}
+	}
+
+}
