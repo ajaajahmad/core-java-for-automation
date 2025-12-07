23dec0cf1e1e665228a907e03cea152bb674bad4
Ajaaj Ahmad <ajaaj75@gmail.com>
2025-12-08 00:02:32 +0530

    exception handling

---
+++ src/main/java/com/java/basics/ExceptionHandling.java
@@ -0,0 +1,16 @@
+package com.java.basics;
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) {
+
+		try {
+
+			int a = 10 / 0;
+			System.out.println(a);
+		} catch (Exception e) {
+			System.out.println("Execption Handalled");
+		}
+	}
+
+}
