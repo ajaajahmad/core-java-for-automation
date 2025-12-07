ce4ba531073bd02bfa9b4971a1e209a7a966d970
Ajaaj Ahmad <ajaaj75@gmail.com>
2025-12-07 22:38:13 +0530

    hashmap

--- src/main/java/com/java/basics/HashMapDemo.java
+++ src/main/java/com/java/basics/HashMapDemo.java
@@ -5,15 +5,19 @@ import java.util.HashMap;
 public class HashMapDemo {
 
 	public static void main(String[] args) {
-		
+
 		HashMap<Integer, String> map = new HashMap<>();
-		
+
 		map.put(1, "Ajaaj");
 		map.put(2, "Ahmad");
 		map.put(3, "Rahul");
-		
-		System.out.println(map.get(1));
-		
+
+		System.out.println(map.size());
+
+		for (Integer i : map.keySet()) {
+			System.out.println(i + " " + map.get(i));
+		}
+
 	}
 
 }
