b4923948049359c8c52141f2a6e1743a1a9efe99
ajaajahmad <ajaaj75@gmail.com>
2025-12-08 18:24:20 +0530

    fibonacci series

---
+++ src/test/java/com/java/tests/FibonacciSeries.java
@@ -0,0 +1,24 @@
+package com.java.tests;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+
+		int a = 0;
+		int b = 1;
+		int sum;
+		int count = 10;
+
+		System.out.print(a + " " + b + " ");
+
+		for (int i = 2; i < count; i++) {
+			sum = a + b;
+			System.out.println(sum + " ");
+
+			a = b;
+			b = sum;
+		}
+
+	}
+
+}
