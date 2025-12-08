306ca634e16ddaef070e03f83ab7e3bd23cd0415
ajaajahmad <ajaaj75@gmail.com>
2025-12-08 22:17:03 +0530

    remove duplicate from an array

---
+++ src/test/java/com/java/interview/RemoveDuplicatesFromArray.java
@@ -0,0 +1,27 @@
+package com.java.interview;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class RemoveDuplicatesFromArray {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 2, 4, 3, 5 };
+
+		HashSet<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			set.add(arr[i]);
+		}
+
+		System.out.println("Array after removing duplicate:");
+		// for (Iterator<Integer> it = set.iterator(); it.hasNext();) {
+		// int num = it.next();
+
+		for (Integer num : set) {
+			System.out.println(num);
+		}
+	}
+
+}
