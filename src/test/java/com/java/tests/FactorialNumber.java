976670f04cce2933d95765f33a1e351821a77441
ajaajahmad <ajaaj75@gmail.com>
2025-12-08 17:59:17 +0530

    factorial number

---
+++ src/test/java/com/java/tests/FactorialNumber.java
@@ -0,0 +1,15 @@
+package com.java.tests;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		int number = 5;
+		int fact = 1;
+
+		for (int i = 1; i <= number; i++) {
+			fact = fact * i;
+		}
+
+		System.out.println(fact);
+	}
+}
