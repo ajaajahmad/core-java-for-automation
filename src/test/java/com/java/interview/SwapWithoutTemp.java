7c3e8681fc8c8cdd7747909bd338467798076a13
ajaajahmad <ajaaj75@gmail.com>
2025-12-08 20:11:31 +0530

    swap without temp

---
+++ src/test/java/com/java/interview/SwapWithoutTemp.java
@@ -0,0 +1,19 @@
+package com.java.interview;
+
+public class SwapWithoutTemp {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+
+		a = a + b;
+		b = a - b;
+		a = a - b;
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+
+	}
+
+}
