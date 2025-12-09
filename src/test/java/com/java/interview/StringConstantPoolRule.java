70122a0aec89550fb52746dccb93c1f5f285b350
Ajaaj Ahmad <ajaaj75@gmail.com>
2025-12-09 23:00:23 +0530

    string constant pool

---
+++ src/test/java/com/java/interview/StringConstantPoolRule.java
@@ -0,0 +1,20 @@
+package com.java.interview;
+
+public class StringConstantPoolRule {
+
+	public static void main(String[] args) {
+
+		String s1 = "hello";
+		String s2 = "hello";
+
+		System.out.println(s1 == s2); // true
+		System.out.println(s1.equals(s2)); // true
+
+		String s3 = new String("hello");
+		String s4 = new String("hello");
+
+		System.out.println(s3 == s4); // false
+		System.out.println(s3.equals(s4)); // true
+	}
+
+}
