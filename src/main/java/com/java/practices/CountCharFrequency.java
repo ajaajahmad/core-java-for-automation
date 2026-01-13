b8077aa1103834562094e244133676e0e4fc4627
ajaajahmad <ajaaj75@gmail.com>
2026-01-13 23:58:59 +0530

    changes

--- src/main/java/com/java/practices/CountCharFrequency.java
+++ src/main/java/com/java/practices/CountCharFrequency.java
@@ -3,8 +3,22 @@ package com.java.practices;
 public class CountCharFrequency {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String str = "test automation engineer";
+		int[] count = new int[127];
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			count[ch]++;
+		}
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (count[ch] > 0) {
+				System.out.println("'" + ch + " :" + count[ch]);
+				count[ch] = 0;
+			}
+		}
 	}
 
 }
